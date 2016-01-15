3a72bd352929bb7970ad7acbc4bba562ae89fd85
Jill Ye <jiyou.linux@gmail.com>
2016-01-15 15:34:06 +0800

    Add codes.

---
+++ fr.js
@@ -0,0 +1,243 @@
+function initBreedSelect(id) {
+  for (var i = 0; i < FRTool.Oddss.length; i++) {
+    var breeds = FRTool.getBreed(FRTool.Oddss[i]);
+    var group = $("<optgroup></optgroup>").attr("label", FRTool.Oddss[i].NameZH + " " + FRTool.Oddss[i].Name);
+    for (var j = 0; j < breeds.length; j++) {
+      group.append($("<option></option>").attr("value", breeds[j].Name).text(breeds[j].NameZH + " " + breeds[j].Name));
+    };
+    $('#' + id).append(group);
+  };
+}
+
+function initGeneSelect(id, type) {
+  for (var i = 0; i < FRTool.Oddss.length; i++) {
+    var genes = FRTool.getGene(FRTool.Oddss[i], type);
+    var group = $("<optgroup></optgroup>").attr("label", FRTool.Oddss[i].NameZH + " " + FRTool.Oddss[i].Name);
+    for (var j = 0; j < genes.length; j++) {
+      group.append($("<option></option>").attr("value", genes[j].Name).text(genes[j].NameZH + " " + genes[j].Name));
+    };
+    $('#' + id).append(group);
+  };
+}
+
+function initColorSelect(id) {
+  for (var i = 0; i < FRTool.Colors.length; i++) {
+    $('#' + id).append(
+      $("<option></option>")
+      .attr("class", FRTool.Colors[i].Name)
+      .attr("value", FRTool.Colors[i].Name)
+      .text(FRTool.Colors[i].NameZH + " " + FRTool.Colors[i].Name)
+    );
+  }
+}
+
+function initGenderSelect(id) {
+  for (var i = 0; i < FRTool.Genders.length; i++) {
+    $('#' + id).append(
+      $("<option></option>")
+      .attr("class", FRTool.Genders[i].Name)
+      .attr("value", FRTool.Genders[i].Name)
+      .text(FRTool.Genders[i].NameZH + " " + FRTool.Genders[i].Name)
+    );
+  }
+}
+
+function UpdateColorClass(colorSelect) {
+  colorSelect.removeClass();
+  colorSelect.addClass("form-control");
+  colorSelect.addClass(colorSelect.val());
+}
+
+function setCookie(cookie, label) {
+  if ($.cookie(cookie)) {
+    $('#' + label).val($.cookie(cookie));
+  }
+}
+
+FRTool.initFR(FRData);
+initBreedSelect('o_breed');
+initBreedSelect('a_breed');
+initBreedSelect('target_Breed');
+initGeneSelect('o_Primary_gene', 'Primary');
+initGeneSelect('a_Primary_gene', 'Primary');
+initGeneSelect('target_primaryGene', 'Primary');
+initGeneSelect('o_Secondary_gene', 'Secondary');
+initGeneSelect('a_Secondary_gene', 'Secondary');
+initGeneSelect('target_secondaryGene', 'Secondary');
+initGeneSelect('o_Tertiary_gene', 'Tertiary');
+initGeneSelect('a_Tertiary_gene', 'Tertiary');
+initGeneSelect('target_tertiaryGene', 'Tertiary');
+initColorSelect('o_primary_color');
+initColorSelect('o_secondary_color');
+initColorSelect('o_tertiary_color');
+initColorSelect('a_primary_color');
+initColorSelect('a_secondary_color');
+initColorSelect('a_tertiary_color');
+initColorSelect('target_primary_Color');
+initColorSelect('target_secondary_Color');
+initColorSelect('target_tertiary_Color');
+initGenderSelect('target_gender');
+
+$("[id$=_Color]").change(function() {
+  UpdateColorClass($(this));
+});
+
+$("[id$=_breed]").change(function() {
+  $('#breed_result').text("...");
+  $.cookie("o_breed", $('#o_breed').val());
+  $.cookie("a_breed", $('#a_breed').val());
+  var s = "";
+  var o_breed = FRTool.Breed[$('#o_breed').val()];
+  var a_breed = FRTool.Breed[$('#a_breed').val()];
+  var rate = FRTool.getBreedRate(o_breed, a_breed);
+  if (rate.length == 1) {
+    s += o_breed.NameZH + " " + o_breed.Name + " " + rate[0] * 100 + "% ";
+    $("#breed_bar_0").text(o_breed.NameZH + " " + o_breed.Name)
+      .css("width", rate[0] * 100 + "% ")
+      .attr("aria-valuenow", rate[0] * 100);
+  } else {
+    s += o_breed.NameZH + " " + o_breed.Name + " " + rate[0] * 100 + "% ";
+    s += a_breed.NameZH + " " + a_breed.Name + " " + rate[1] * 100 + "% ";
+    $("#breed_bar_0").text(o_breed.NameZH + " " + o_breed.Name)
+      .css("width", rate[0] * 100 + "% ")
+      .attr("aria-valuenow", rate[0] * 100);
+    $("#breed_bar_1").text(a_breed.NameZH + " " + a_breed.Name)
+      .css("width", rate[1] * 100 + "% ")
+      .attr("aria-valuenow", rate[1] * 100);
+
+  }
+
+  $('#breed_result').text(s);
+});
+
+$("[id$=_gene]").change(function() {
+  var gene_type = $(this).attr('id').split('_')[1];
+  $.cookie('o_' + gene_type, $('#o_' + gene_type + '_gene').val());
+  $.cookie('a_' + gene_type, $('#a_' + gene_type + '_gene').val());
+  $('#' + gene_type + '_result').text("...");
+  var s = "";
+  var o_gene = FRTool[gene_type + "Gene"][$('#o_' + gene_type + '_gene').val()];
+  var a_gene = FRTool[gene_type + "Gene"][$('#a_' + gene_type + '_gene').val()];
+  var rate = FRTool.getGeneRate(o_gene, a_gene);
+  if (rate.length == 1) {
+    s += o_gene.NameZH + " " + o_gene.Name + " " + rate[0] * 100 + "% ";
+    $("#gene_" + gene_type + "_bar_0").text(o_gene.NameZH + " " + o_gene.Name)
+      .css("width", rate[0] * 100 + "% ")
+      .attr("aria-valuenow", rate[0] * 100);
+  } else {
+    s += o_gene.NameZH + " " + o_gene.Name + " " + rate[0] * 100 + "% ";
+    s += a_gene.NameZH + " " + a_gene.Name + " " + rate[1] * 100 + "% ";
+    $("#gene_" + gene_type + "_bar_0").text(o_gene.NameZH + " " + o_gene.Name)
+      .css("width", rate[0] * 100 + "% ")
+      .attr("aria-valuenow", rate[0] * 100);
+    $("#gene_" + gene_type + "_bar_1").text(a_gene.NameZH + " " + a_gene.Name)
+      .css("width", rate[1] * 100 + "% ")
+      .attr("aria-valuenow", rate[1] * 100);
+
+  }
+
+  $('#' + gene_type + '_result').text(s);
+});
+
+$("[id$=_color]").change(function() {
+  UpdateColorClass($(this));
+  var color_type = $(this).attr('id').split('_')[1];
+  $.cookie('o_' + color_type + '_color', $('#o_' + color_type + '_color').val());
+  $.cookie('a_' + color_type + '_color', $('#a_' + color_type + '_color').val());
+  var s = "";
+  var o_color = FRTool.Color[$('#o_' + color_type + '_color').val()];
+  var a_color = FRTool.Color[$('#a_' + color_type + '_color').val()];
+  var rate = FRTool.getColorRange(o_color, a_color);
+  $('#' + color_type + '_color_result').text("");
+  for (var i = 0; i < rate.length; i++) {
+    var item = rate[i];
+    var row = $("<span></span>")
+      .attr("title", item.NameZH + " " + item.Name)
+      .attr("data-toggle", "tooltip")
+      .attr("data-placement", "bottom")
+      .css("width", "7.5px")
+      .css("height", "20px")
+      .css("display", "inline-block")
+      .css("background-color", item.Color);
+    $('#' + color_type + '_color_result').append(row);
+  };
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$("#cal_breed").click(function() {
+  var $btn = $(this).button('loading');
+  $('#input_result').text('--.-------- %');
+  var target = {};
+  $('#input_result').val("");
+  if ($('#target_gender').val() != "none") {
+    target['gender'] = $('#target_gender').val();
+  }
+  if ($('#target_Breed').val() != "none") {
+    target['breed'] = $('#target_Breed').val();
+  }
+  if ($('#target_primary_Color').val() != "none") {
+    target['primaryColor'] = $('#target_primary_Color').val();
+  }
+  if ($('#target_secondary_Color').val() != "none") {
+    target['secondaryColor'] = $('#target_secondary_Color').val();
+  }
+  if ($('#target_tertiary_Color').val() != "none") {
+    target['tertiaryColor'] = $('#target_tertiary_Color').val();
+  }
+  if ($('#target_primaryGene').val() != "none") {
+    target['primaryGene'] = $('#target_primaryGene').val();
+  }
+  if ($('#target_secondaryGene').val() != "none") {
+    target['secondaryGene'] = $('#target_secondaryGene').val();
+  }
+  if ($('#target_tertiaryGene').val() != "none") {
+    target['tertiaryGene'] = $('#target_tertiaryGene').val();
+  }
+  var info1 = {
+    'breed': $('#o_breed').val(),
+    'primaryColor':$('#o_primary_color').val(),
+    'secondaryColor':$('#o_secondary_color').val(),
+    'tertiaryColor':$('#o_tertiary_color').val(),
+    'primaryGene':$('#o_Primary_gene').val(),
+    'secondaryGene':$('#o_Secondary_gene').val(),
+    'tertiaryGene':$('#o_Tertiary_gene').val()
+  };
+  var info2 = {
+    'breed': $('#a_breed').val(),
+    'primaryColor':$('#a_primary_color').val(),
+    'secondaryColor':$('#a_secondary_color').val(),
+    'tertiaryColor':$('#a_tertiary_color').val(),
+    'primaryGene':$('#a_Primary_gene').val(),
+    'secondaryGene':$('#a_Secondary_gene').val(),
+    'tertiaryGene':$('#a_Tertiary_gene').val()
+  };
+  $('#input_result').text((FRTool.calRate(info1,info2,target) * 100).toFixed(8) + " %");
+  $btn.button('reset');
+});
+
+$('[data-toggle="tooltip"]').tooltip();
+
+$('[data-toggle="tooltip"]').tooltip();
+
+setCookie("o_breed", "o_breed");
+setCookie("a_breed", "a_breed");
+
+for (var i = $("[id$=_gene]").length - 1; i >= 0; i--) {
+  var form = $($("[id$=_gene]")[i]);
+  setCookie(form.attr('id').substring(0, form.attr('id').length - 5), form.attr('id'));
+};
+
+for (var i = $("[id$=_color]").length - 1; i >= 0; i--) {
+  var form = $($("[id$=_color]")[i]);
+  setCookie(form.attr('id'), form.attr('id'));
+};
+
+for (var i = $("[id^=target_]").length - 1; i >= 0; i--) {
+  var form = $($("[id^=target_]")[i]);
+  setCookie(form.attr('id'), form.attr('id'));
+};
+
+$("[id$=_color]").change();
+$("[id$=_Color]").change();
+$("[id$=_breed]").change();
+$("[id$=_gene]").change();
---
+++ fr_tool.js
@@ -0,0 +1,287 @@
+FRTool = {}
+
+FRTool.Odds = function(name, name_zh) {
+    this.Name = name;
+    this.NameZH = name_zh;
+    FRTool.Odds[name] = this;
+    FRTool.Oddss.push(this);
+}
+
+FRTool.Gender = function(name, name_zh) {
+    this.Name = name;
+    this.NameZH = name_zh;
+    FRTool.Gender[name] = this;
+    FRTool.Genders.push(this);
+}
+
+FRTool.PrimaryGene = function(name, name_zh, odds) {
+    this.Name = name;
+    this.NameZH = name_zh;
+    this.Odds = FRTool.Odds[odds];
+    FRTool.PrimaryGene[name] = this;
+    FRTool.PrimaryGenes.push(this);
+}
+
+FRTool.SecondaryGene = function(name, name_zh, odds) {
+    this.Name = name;
+    this.NameZH = name_zh;
+    this.Odds = FRTool.Odds[odds];
+    FRTool.SecondaryGene[name] = this;
+    FRTool.SecondaryGenes.push(this);
+}
+
+FRTool.TertiaryGene = function(name, name_zh, odds) {
+    this.Name = name;
+    this.NameZH = name_zh;
+    this.Odds = FRTool.Odds[odds];
+    FRTool.TertiaryGene[name] = this;
+    FRTool.TertiaryGenes.push(this);
+}
+
+FRTool.Color = function(name, name_zh, color) {
+    this.Name = name;
+    this.NameZH = name_zh;
+    this.Color = color;
+    this.Id = function() {
+        return FRTool.Colors.indexOf(this) + 1;
+    }
+    FRTool.Color[name] = this;
+    FRTool.Colors.push(this);
+}
+
+FRTool.Breed = function(name, name_zh, odds, diet) {
+    this.Name = name;
+    this.NameZH = name_zh;
+    this.Odds = FRTool.Odds[odds];
+    this.Diet = diet;
+    FRTool.Breed[name] = this;
+    FRTool.Breeds.push(this);
+}
+
+FRTool.initFR = function(data) {
+    FRTool.Genders = [];
+    FRTool.Oddss = [];
+    FRTool.PrimaryGenes = [];
+    FRTool.SecondaryGenes = [];
+    FRTool.TertiaryGenes = [];
+    FRTool.Breeds = [];
+    FRTool.Colors = [];
+    FRTool.BreedRoles = {};
+    FRTool.GeneRoles = {};
+    for (var i = 0; i < data.OddsList.length; i++) {
+        new FRTool.Odds(data.OddsList[i][0], data.OddsList[i][1]);
+    };
+
+    for (var i = 0; i < data.GenderList.length; i++) {
+        new FRTool.Gender(data.GenderList[i][0], data.GenderList[i][1]);
+    };
+
+    for (var i = 0; i < data.PrimaryGeneList.length; i++) {
+        new FRTool.PrimaryGene(data.PrimaryGeneList[i][0], data.PrimaryGeneList[i][1], data.PrimaryGeneList[i][2]);
+    };
+
+    for (var i = 0; i < data.SecondaryGeneList.length; i++) {
+        new FRTool.SecondaryGene(data.SecondaryGeneList[i][0], data.SecondaryGeneList[i][1], data.SecondaryGeneList[i][2]);
+    };
+
+    for (var i = 0; i < data.TertiaryGeneList.length; i++) {
+        new FRTool.TertiaryGene(data.TertiaryGeneList[i][0], data.TertiaryGeneList[i][1], data.TertiaryGeneList[i][2]);
+    };
+
+    for (var i = 0; i < data.ColorList.length; i++) {
+        new FRTool.Color(data.ColorList[i][0], data.ColorList[i][1], data.ColorList[i][2]);
+    };
+
+    for (var i = data.BreedRuleList.length - 1; i >= 0; i--) {
+        FRTool.BreedRoles[data.BreedRuleList[i][0]] = data.BreedRuleList[i][1];
+    };
+
+    for (var i = data.GeneRuleList.length - 1; i >= 0; i--) {
+        FRTool.GeneRoles[data.GeneRuleList[i][0]] = data.GeneRuleList[i][1];
+    };
+
+    for (var i = 0; i < data.BreedList.length; i++) {
+        new FRTool.Breed(data.BreedList[i][0], data.BreedList[i][1], data.BreedList[i][2], data.BreedList[i][3]);
+    };
+}
+
+FRTool.getColorRange = function(color1, color2) {
+    if (color1 instanceof FRTool.Color && color2 instanceof FRTool.Color) {
+        if (color1 == color2) {
+            return [color1];
+        }
+
+        var small = color1;
+        var big = color2;
+        if (color1.Id() > color2.Id()) {
+            small = color2;
+            big = color1;
+        }
+
+        left = FRTool.Colors.slice(0, small.Id() - 1);
+        center = FRTool.Colors.slice(small.Id(), big.Id() - 1);
+        right = FRTool.Colors.slice(big.Id());
+
+
+        if (big.Id() - small.Id() < FRTool.Colors.length / 2) {
+            return [small].concat(center).concat([big]);
+        } else {
+            return [big].concat(right).concat(left).concat(small);
+        }
+    }
+    return null;
+}
+
+FRTool.getBreedRate = function(breed1, breed2) {
+    if (breed1 instanceof FRTool.Breed && breed2 instanceof FRTool.Breed) {
+        if (breed1 == breed2) {
+            return [1];
+        }
+        if (FRTool.BreedRoles[breed1.Odds.Name + ',' + breed2.Odds.Name] != undefined) {
+            return [FRTool.BreedRoles[breed1.Odds.Name + ',' + breed2.Odds.Name][0], FRTool.BreedRoles[breed1.Odds.Name + ',' + breed2.Odds.Name][1]];
+        } else if (FRTool.BreedRoles[breed2.Odds.Name + ',' + breed1.Odds.Name] != undefined) {
+            return [FRTool.BreedRoles[breed2.Odds.Name + ',' + breed1.Odds.Name][1], FRTool.BreedRoles[breed2.Odds.Name + ',' + breed1.Odds.Name][0]];
+        }
+    }
+
+    return [];
+}
+
+FRTool.getBreed = function(odds) {
+    var breeds = [];
+    for (var i = 0; i < FRTool.Breeds.length; i++) {
+        if (FRTool.Breeds[i].Odds == odds) {
+            breeds.push(FRTool.Breeds[i]);
+        }
+    };
+    return breeds;
+}
+
+FRTool.getGene = function(odds, type) {
+    var genes = [];
+    for (var i = 0; i < FRTool[type + "Genes"].length; i++) {
+        if (FRTool[type + "Genes"][i].Odds == odds) {
+            genes.push(FRTool[type + "Genes"][i]);
+        }
+    };
+
+    return genes;
+}
+
+FRTool.getGeneRate = function(gene1, gene2) {
+    if ((gene1 instanceof FRTool.PrimaryGene && gene2 instanceof FRTool.PrimaryGene) 
+        || (gene1 instanceof FRTool.SecondaryGene && gene2 instanceof FRTool.SecondaryGene) 
+        || (gene1 instanceof FRTool.TertiaryGene && gene2 instanceof FRTool.TertiaryGene)) {
+        if (gene1 == gene2) {
+            return [1];
+        }
+        if (FRTool.GeneRoles[gene1.Odds.Name + ',' + gene2.Odds.Name] != undefined) {
+            return [FRTool.GeneRoles[gene1.Odds.Name + ',' + gene2.Odds.Name][0], FRTool.GeneRoles[gene1.Odds.Name + ',' + gene2.Odds.Name][1]];
+        } else if (FRTool.GeneRoles[gene2.Odds.Name + ',' + gene1.Odds.Name] != undefined) {
+            return [FRTool.GeneRoles[gene2.Odds.Name + ',' + gene1.Odds.Name][1], FRTool.GeneRoles[gene2.Odds.Name + ',' + gene1.Odds.Name][0]];
+        }
+    }
+
+    return [];
+}
+
+FRTool.calRate = function (info1, info2, target) {
+    var result = 1.0
+    if (target.gender != undefined) {
+        result *= 0.5;
+    }
+
+    if (target.breed != undefined) {
+        var breed = FRTool.Breed[target.breed]
+        var breed1 = FRTool.Breed[info1.breed];
+        var breed2 = FRTool.Breed[info2.breed];
+        var rate = FRTool.getBreedRate(breed1, breed2);
+        if (breed == breed1) {
+            result *= rate[0];
+        } else if(breed == breed2) {
+            result *= rate[1];
+        } else {
+            return 0;
+        }
+    }
+
+    if (target.primaryColor != undefined) {
+        var primaryColor = FRTool.Color[target.primaryColor]
+        var primaryColor1 = FRTool.Color[info1.primaryColor];
+        var primaryColor2 = FRTool.Color[info2.primaryColor];
+        var range = FRTool.getColorRange(primaryColor1, primaryColor2);
+        if (range.indexOf(primaryColor) >= 0) {
+            result /= range.length;
+        } else {
+            return 0;
+        }
+    }
+
+    if (target.secondaryColor != undefined) {
+        var secondaryColor = FRTool.Color[target.secondaryColor]
+        var secondaryColor1 = FRTool.Color[info1.secondaryColor];
+        var secondaryColor2 = FRTool.Color[info2.secondaryColor];
+        var range = FRTool.getColorRange(secondaryColor1, secondaryColor2);
+        if (range.indexOf(secondaryColor) >= 0) {
+            result /= range.length;
+        } else {
+            return 0;
+        }
+    }
+
+    if (target.tertiaryColor != undefined) {
+        var tertiaryColor = FRTool.Color[target.tertiaryColor]
+        var tertiaryColor1 = FRTool.Color[info1.tertiaryColor];
+        var tertiaryColor2 = FRTool.Color[info2.tertiaryColor];
+        var range = FRTool.getColorRange(tertiaryColor1, tertiaryColor2);
+        if (range.indexOf(tertiaryColor) >= 0) {
+            result /= range.length;
+        } else {
+            return 0;
+        }
+    }
+
+    if (target.primaryGene != undefined) {
+        var primaryGene = FRTool.PrimaryGene[target.primaryGene]
+        var primaryGene1 = FRTool.PrimaryGene[info1.primaryGene];
+        var primaryGene2 = FRTool.PrimaryGene[info2.primaryGene];
+        var rate = FRTool.getGeneRate(primaryGene1, primaryGene2);
+        if (primaryGene == primaryGene1) {
+            result *= rate[0];
+        } else if(primaryGene == primaryGene2) {
+            result *= rate[1];
+        } else {
+            return 0;
+        }
+    }
+
+    if (target.secondaryGene != undefined) {
+        var secondaryGene = FRTool.SecondaryGene[target.secondaryGene]
+        var secondaryGene1 = FRTool.SecondaryGene[info1.secondaryGene];
+        var secondaryGene2 = FRTool.SecondaryGene[info2.secondaryGene];
+        var rate = FRTool.getGeneRate(secondaryGene1, secondaryGene2);
+        if (secondaryGene == secondaryGene1) {
+            result *= rate[0];
+        } else if(secondaryGene == secondaryGene2) {
+            result *= rate[1];
+        } else {
+            return 0;
+        }
+    }
+
+    if (target.tertiaryGene != undefined) {
+        var tertiaryGene = FRTool.TertiaryGene[target.tertiaryGene]
+        var tertiaryGene1 = FRTool.TertiaryGene[info1.tertiaryGene];
+        var tertiaryGene2 = FRTool.TertiaryGene[info2.tertiaryGene];
+        var rate = FRTool.getGeneRate(tertiaryGene1, tertiaryGene2);
+        if (tertiaryGene == tertiaryGene1) {
+            result *= rate[0];
+        } else if(tertiaryGene == tertiaryGene2) {
+            result *= rate[1];
+        } else {
+            return 0;
+        }
+    }
+ 
+    return result
+}
