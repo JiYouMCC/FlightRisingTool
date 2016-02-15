3670d97dd623a9fb22a242b0b7110eef56ab8fb3
Jill Ye <jiyou.linux@gmail.com>
2016-02-15 14:13:08 +0800

    update nav style

--- js/data.js
+++ js/data.js
@@ -1,4 +1,3 @@
-$('[data-toggle="tooltip"]').tooltip();
 FRTool.initFR(FRData);
 
 for (var i = 0; i < FRTool.Genders.length; i++) {
@@ -6,7 +5,7 @@ for (var i = 0; i < FRTool.Genders.length; i++) {
         $("<tr></tr>")
         .append($("<td></td>").text(FRTool.Genders[i].NameZH))
         .append($("<td></td>").text(FRTool.Genders[i].Name))
-        );
+    );
 }
 
 for (var i = 0; i < FRTool.Oddss.length; i++) {
@@ -15,7 +14,7 @@ for (var i = 0; i < FRTool.Oddss.length; i++) {
         .append($("<td></td>").text(FRTool.Oddss[i].NameZH))
         .append($("<td></td>").text(FRTool.Oddss[i].Name))
         .append($("<td></td>").text(FRTool.Oddss[i].CoolDown))
-        );
+    );
 }
 
 for (var i = 0; i < FRTool.Breeds.length; i++) {
@@ -25,7 +24,7 @@ for (var i = 0; i < FRTool.Breeds.length; i++) {
         .append($("<td></td>").text(FRTool.Breeds[i].Name))
         .append($("<td></td>").text(FRTool.Breeds[i].Odds.NameZH))
         .append($("<td></td>").text(FRTool.Breeds[i].Odds.Name))
-        );
+    );
 }
 
 for (var i = 0; i < FRTool.PrimaryGenes.length; i++) {
@@ -35,7 +34,7 @@ for (var i = 0; i < FRTool.PrimaryGenes.length; i++) {
         .append($("<td></td>").text(FRTool.PrimaryGenes[i].Name))
         .append($("<td></td>").text(FRTool.PrimaryGenes[i].Odds.NameZH))
         .append($("<td></td>").text(FRTool.PrimaryGenes[i].Odds.Name))
-        );
+    );
 }
 
 for (var i = 0; i < FRTool.SecondaryGenes.length; i++) {
@@ -45,7 +44,7 @@ for (var i = 0; i < FRTool.SecondaryGenes.length; i++) {
         .append($("<td></td>").text(FRTool.SecondaryGenes[i].Name))
         .append($("<td></td>").text(FRTool.SecondaryGenes[i].Odds.NameZH))
         .append($("<td></td>").text(FRTool.SecondaryGenes[i].Odds.Name))
-        );
+    );
 }
 
 for (var i = 0; i < FRTool.TertiaryGenes.length; i++) {
@@ -55,7 +54,7 @@ for (var i = 0; i < FRTool.TertiaryGenes.length; i++) {
         .append($("<td></td>").text(FRTool.TertiaryGenes[i].Name))
         .append($("<td></td>").text(FRTool.TertiaryGenes[i].Odds.NameZH))
         .append($("<td></td>").text(FRTool.TertiaryGenes[i].Odds.Name))
-        );
+    );
 }
 
 for (var i = 0; i < FRTool.Colors.length; i++) {
@@ -64,7 +63,7 @@ for (var i = 0; i < FRTool.Colors.length; i++) {
         .append($("<td></td>").text(FRTool.Colors[i].NameZH))
         .append($("<td></td>").text(FRTool.Colors[i].Name))
         .append($("<td></td>").attr('class', FRTool.Colors[i].Name).text(FRTool.Colors[i].Color))
-        );
+    );
 }
 
 for (key in FRTool.BreedRoles) {
@@ -80,7 +79,7 @@ for (key in FRTool.BreedRoles) {
         .append($("<td></td>").text(odds2.Name))
         .append($("<td></td>").text(rate1))
         .append($("<td></td>").text(rate2))
-        );
+    );
 }
 
 for (key in FRTool.GeneRoles) {
@@ -96,37 +95,47 @@ for (key in FRTool.GeneRoles) {
         .append($("<td></td>").text(odds2.Name))
         .append($("<td></td>").text(rate1))
         .append($("<td></td>").text(rate2))
-        );
+    );
 }
 
 $("body").attr("data-spy", "scroll");
 $("body").attr("data-target", "#affix-nav");
-for (var i = 0; i < $("h4").length ; i ++) {
-    $($("h4")[i]).attr("id", "title_"+i);
-        $("<li></li>").append(
+for (var i = 0; i < $("h4").length; i++) {
+    $($("h4")[i]).attr("id", "title_" + i);
+    var title = $($("h4")[i]).text().split(' ');
+    var text = title.shift();
+    title = title.join(" ");
+    $("<li></li>").append(
             $("<a></a>")
             .attr("href", "#title_" + i)
-            .text($($("h4")[i]).text())
-            ).insertBefore('#nav_bottom');
+            .text(text)
+        )
+        .attr("data-toggle", "tooltip")
+        .attr("data-placement", "left")
+        .attr("title", title)
+        .insertBefore('#nav_bottom');
 }
 
 $('#affix-nav').affix({
-  offset: {
-    top: function() {
-      return (this.top = $('#top').outerHeight(true));
-    },
-    bottom: function () {
-      return (this.bottom = $('#bottom').outerHeight(true) + 20);
+    offset: {
+        top: function() {
+            return (this.top = $('#top').outerHeight(true));
+        },
+        bottom: function() {
+            return (this.bottom = $('#bottom').outerHeight(true) + 20);
+        }
     }
-  }
 })
 
-$('.affix-top').width($('.affix-top').width());
+$('.affix-top').width($('#stand').width());
+$('.affix').width($('#stand').width());
 
-$(window).resize(function () {
-    $('.affix').width($('.affix-top').width());
+$(window).resize(function() {
+    $('.affix').width($('#stand').width());
 });
 
-$(window).scroll(function () {
-    $('.affix').width($('.affix-top').width());
+$(window).scroll(function() {
+    $('.affix').width($('#stand').width());
 });
+
+$('[data-toggle="tooltip"]').tooltip();
