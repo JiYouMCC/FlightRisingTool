45f630d2b85e906098118570791de1b6f3ff45d9
Jill Ye <nightghostjiyou@gmail.com>
2016-03-02 18:05:58 +0800

    Update language.js

--- js/language.js
+++ js/language.js
@@ -22,7 +22,7 @@ for (var i = 0; i < language_setting.languages.length; i++) {
     );
 }
 
-var language_code = navigator.language;
+var language_code = navigator.language.split('_')[0];
 
 $(document).ready(function(){
     if ($.cookie("local_language_code")) {
